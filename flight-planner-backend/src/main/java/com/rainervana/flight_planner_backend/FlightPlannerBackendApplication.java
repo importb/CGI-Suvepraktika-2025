1fb6becd44ee66d3b14c034e34350f64f8c427e4
importb <rainervana1@gmail.com>
2025-03-29 10:50:51 +0200

    Initial SpringBoot backend

---
+++ flight-planner-backend/src/main/java/com/rainervana/flight_planner_backend/FlightPlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rainervana.flight_planner_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPlannerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightPlannerBackendApplication.class, args);
+	}
+
+}
---
+++ flight-planner-backend/src/test/java/com/rainervana/flight_planner_backend/FlightPlannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainervana.flight_planner_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPlannerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
